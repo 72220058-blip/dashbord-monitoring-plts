4b07bc3eba691f75eacdac4e4266f316121c6f3e
72220058-blip <72220058@bsi.ac.id>
2026-01-22 19:46:02 +0700

    Add files via upload

---
+++ monitoring.js
@@ -0,0 +1,40 @@
+const client = new Paho.MQTT.Client(
+  "broker.hivemq.com",
+  8884,
+  "dashboard-" + Math.random()
+);
+
+client.onConnectionLost = () => {
+  document.getElementById("mqtt").innerText = "MQTT Disconnected";
+};
+
+client.onMessageArrived = (message) => {
+  if (message.destinationName === "monitor/relay") {
+    const status = message.payloadString;
+    const el = document.getElementById("status");
+
+    if (status === "ON") {
+      el.innerHTML = "PLTS ON";
+      el.className = "status plts";
+    } else {
+      el.innerHTML = "PLN ON";
+      el.className = "status pln";
+    }
+  }
+};
+
+client.connect({
+  useSSL: true,
+  onSuccess: () => {
+    document.getElementById("mqtt").innerText = "MQTT Connected";
+    client.subscribe("monitor/relay");
+  }
+});
+
+function relayON() {
+  client.send("kontrol/relay", "ON");
+}
+
+function relayOFF() {
+  client.send("kontrol/relay", "OFF");
+}
